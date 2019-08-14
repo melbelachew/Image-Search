b963196edfdc4c3df65363b23255d1001f0f0aa1
Mel Belachew <melbelachew@gmail.com>
2019-08-14 13:03:35 -0400

    Updates

---
+++ precache-manifest.42a5152d680b0f1de5773e7a3e541106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b5aa295c3e34fabf196e27068807c4",
+    "url": "/Image-Search/index.html"
+  },
+  {
+    "revision": "ad9129197f44da5724d8",
+    "url": "/Image-Search/static/css/main.ac090118.chunk.css"
+  },
+  {
+    "revision": "bfcf9516b91892522f9e",
+    "url": "/Image-Search/static/js/2.8e15f14f.chunk.js"
+  },
+  {
+    "revision": "ad9129197f44da5724d8",
+    "url": "/Image-Search/static/js/main.2308f8e0.chunk.js"
+  },
+  {
+    "revision": "212f7a5e9c84e343eaf6",
+    "url": "/Image-Search/static/js/runtime~main.6591d98b.js"
+  }
+]);
